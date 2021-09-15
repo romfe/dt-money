b3c60977539c554144fe0f87aee6518b3ee05da3
Romulo Fernandes Evangelista <romfe89@gmail.com>
2021-09-15 17:23:33 -0300

    tabela principal concluída

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,21 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import { api } from '../../services/api';
 import { Container } from "./styles";
 
+
+interface Transaction{
+  id:number;
+  title:string;
+  amount:number;
+  type:string;
+  category:string;
+  createdAt:string;
+}
 export const TransactionsTable = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
   useEffect(() =>{
     api.get('transactions')
-    .then(response => console.log(response.data))
+    .then(response => setTransactions(response.data.transactions))
   }, []);
   return (
     <Container>
@@ -19,18 +29,21 @@ export const TransactionsTable = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021 </td>
+         {transactions.map(transaction =>
+          (<tr key={transaction.id}>
+            <td>{transaction.title}</td>
+            <td className={transaction.type}>
+              {new Intl.NumberFormat('pt-br', {
+              style:'currency',
+              currency:'BRL'
+              }).format(transaction.amount)}
+            </td>
+            <td>{transaction.category}</td>
+            <td>{new Intl.DateTimeFormat('pt-br').format(
+              new Date(transaction.createdAt)
+            )}</td>
           </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">- R$1.100</td>
-            <td>Casa</td>
-            <td>20/02/2021 </td>
-          </tr>
+           ))}
         </tbody>
       </table>
     </Container>
